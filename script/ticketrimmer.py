6ad7912dc113dac40ef73fee21e3de90dc978f24
chick-b <chick-b@users.noreply.github.com>
2016-05-28 03:16:03 +0900

    add script/priticketrimmer.py

---
+++ script/ticketrimmer.py
@@ -0,0 +1,11 @@
+import os, sys
+
+argv = sys.argv
+
+inputpath = os.path.abspath(argv[1])
+base, ext = os.path.splitext(inputpath)
+outputpath = inputpath.replace(ext, '_crop' + ext)
+
+import subprocess
+cmd = ['magick', 'convert', inputpath, '-crop', '720x471+181+279', outputpath ]
+subprocess.run(cmd)
