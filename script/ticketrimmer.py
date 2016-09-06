d526db8c846de1692c5fcacd7b72d47b91f73e9d
chick-b <chick-b@users.noreply.github.com>
2016-09-07 01:16:52 +0900

    add priticket trimming profile

--- script/ticketrimmer.py
+++ script/ticketrimmer.py
@@ -7,5 +7,6 @@ base, ext = os.path.splitext(inputpath)
 outputpath = inputpath.replace(ext, '_crop' + ext)
 
 import subprocess
-cmd = ['magick', 'convert', inputpath, '-crop', '720x471+181+279', outputpath ]
+cmd = ['magick', 'convert', inputpath, '-crop', '720x1498+181+279', outputpath ]
+# cmd = ['magick', 'convert', inputpath, '-crop', '720x471+181+279', outputpath ]
 subprocess.run(cmd)
